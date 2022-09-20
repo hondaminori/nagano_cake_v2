d84580e5acb50e9bac278cfbd5b5a3ae1bf74d0c
hondaminori <hondaminori.ta3@gmail.com>
2022-09-21 02:43:16 +0900

    [3.1.2 Start] ジャンル一覧・追加画面 モデル、テストデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Admin.create!(
-  email: "admin@admin.com",
-  password: "passw0rd"
+Genre.create!(
+  [
+    {
+      name: "ケーキ"
+    },
+    {
+      name: "プリン"
+    },
+    {
+      name: "焼き菓子"
+    },
+    {
+      name: "キャンディ"
+    }
+  ]
 )
