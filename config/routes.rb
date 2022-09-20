c0e5d5c66e68a7c9a113050290c1f7c5012a724f
hondaminori <hondaminori.ta3@gmail.com>
2022-09-21 03:09:59 +0900

    [3.1.2 Update] ジャンル一覧・追加画面 ビュー表示

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    resources :genres
+  end
 end
